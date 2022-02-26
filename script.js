347f0c09f8554b74b6365d75175a567ee0fb0c8f
paulboguta <paul.boguta@gmail.com>
2022-02-26 16:58:06 +0100

    create calc function

--- script.js
+++ script.js
@@ -3,7 +3,47 @@ const displayCalc = document.querySelector(".container__display-calc");
 const displayOperations = document.querySelector(
   ".container__display-operations"
 );
+const keys = document.querySelectorAll(".key");
+const keyNumber = document.querySelectorAll(".key-num");
+const keyOperator = document.querySelectorAll(".key-operator");
+const keyReset = document.querySelector(".key-reset");
 
+keys.forEach((key) => {
+  key.addEventListener("click", function (e) {
+   
+  
+    
+});
+
+// CALCULATION FUNCTIONS
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let a = "",
+  b = 1,
+  operator = "",
+  calc = 0;
+
+// RESETING CALCULATION
+keyReset.addEventListener("click", function (e) {
+  clear();
+});
+
+// HANDLING DISPLAYING OPERATIONS
 keyWhite.forEach((key) => {
   key.addEventListener("click", function (e) {
     appendNumber(key.textContent);
@@ -15,3 +55,12 @@ function appendNumber(number) {
   displayOperations.textContent += " ";
 }
 
+// FUNCTION CLEARING CALCULATOR
+function clear() {
+  a = "";
+  b = "";
+  operator = "";
+  calc = 0;
+  displayCalc.textContent = "";
+  displayOperations.textContent = "";
+}
