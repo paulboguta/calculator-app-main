cc191dac2286eaf3d082f6327cced52b630e7d6b
paulboguta <paul.boguta@gmail.com>
2022-02-27 18:49:35 +0100

    finished calculator
    yaay

--- script.js
+++ script.js
@@ -1,66 +1,186 @@
-const keyWhite = document.querySelectorAll(".key-white");
 const displayCalc = document.querySelector(".container__display-calc");
 const displayOperations = document.querySelector(
   ".container__display-operations"
 );
-const keys = document.querySelectorAll(".key");
+
 const keyNumber = document.querySelectorAll(".key-num");
 const keyOperator = document.querySelectorAll(".key-operator");
 const keyReset = document.querySelector(".key-reset");
+const keyDel = document.querySelector(".key-del");
+const keyEqual = document.querySelector(".key-equal");
 
-keys.forEach((key) => {
-  key.addEventListener("click", function (e) {
-   
-  
-    
-});
+let a = "", // current operation
+  b = "", // previous operation
+  operator = undefined; //operator
 
-// CALCULATION FUNCTIONS
+// CALCULATE FUNCTION
 
-function add(a, b) {
-  return a + b;
-}
+const calculate = () => {
+  let calc;
+  if (!a || !b) return;
 
-function substract(a, b) {
-  return a - b;
-}
+  let currCalc = parseFloat(a);
+  let prevCalc = parseFloat(b);
 
-function multiply(a, b) {
-  return a * b;
-}
+  if (isNaN(currCalc) || isNaN(prevCalc)) return;
 
-function divide(a, b) {
-  return a / b;
-}
+  switch (operator) {
+    case "+":
+      calc = prevCalc + currCalc;
+      break;
 
-let a = "",
-  b = 1,
-  operator = "",
-  calc = 0;
+    case "-":
+      calc = prevCalc - currCalc;
+      break;
 
-// RESETING CALCULATION
-keyReset.addEventListener("click", function (e) {
-  clear();
-});
+    case "x":
+      calc = prevCalc * currCalc;
+      break;
 
-// HANDLING DISPLAYING OPERATIONS
-keyWhite.forEach((key) => {
-  key.addEventListener("click", function (e) {
-    appendNumber(key.textContent);
+    case "/":
+      calc = prevCalc / currCalc;
+      break;
+    default:
+      return;
+  }
+
+  a = calc;
+  operator = undefined;
+  b = "";
+};
+
+// HANDLING GETTING OPERATOR
+keyOperator.forEach((op) => {
+  op.addEventListener("click", () => {
+    getOperator(op.textContent);
+    displayCalculation();
   });
 });
 
-function appendNumber(number) {
-  displayOperations.textContent += number;
-  displayOperations.textContent += " ";
-}
+const getOperator = (op) => {
+  if (a === "") {
+    return;
+  }
+
+  if (b !== "") {
+    calculate();
+  }
+
+  operator = op;
+  b = a;
+  a = "";
+};
+
+// HANDLING DISPLAYING OPERATIONS
+
+const displayCalculation = () => {
+  displayCalc.textContent = a;
+  displayOperations.textContent = b;
+
+  if (operator != null) {
+    displayOperations.textContent = b + operator;
+  } else {
+    displayOperations.textContent = "";
+  }
+};
+
+const addNumber = (number) => {
+  if (number === ".") {
+    if (a.includes(".")) {
+      return;
+    }
+  }
+  a = a.toString() + number.toString();
+};
+
+keyNumber.forEach((number) => {
+  number.addEventListener("click", () => {
+    addNumber(number.textContent);
+    displayCalculation();
+  });
+});
+
+// HANDLING EQUAL (=) BUTTON
+
+keyEqual.addEventListener("click", () => {
+  calculate();
+  displayCalculation();
+});
+
+///// HANDLING RESET AND DELETE BUTTON
 
 // FUNCTION CLEARING CALCULATOR
-function clear() {
+keyReset.addEventListener("click", () => {
+  clear();
+});
+
+const clear = () => {
   a = "";
   b = "";
   operator = "";
-  calc = 0;
   displayCalc.textContent = "";
   displayOperations.textContent = "";
+};
+
+// DELETING LAST DIGIT
+keyDel.addEventListener("click", () => {
+  deleteDigit();
+  displayCalculation();
+});
+
+const deleteDigit = () => {
+  a = a.toString().slice(0, -1);
+};
+
+// HANDLING THEMES
+
+const themeTitle = document.querySelector(".theme-selection__title");
+const themeNumber = document.querySelectorAll(".theme-selection__number");
+const logo = document.querySelector(".container__top-bar--logo");
+
+const toggle1 = document.getElementById("toggle1");
+const toggle2 = document.getElementById("toggle2");
+const toggle3 = document.getElementById("toggle3");
+
+document.documentElement.setAttribute("data-theme", "1");
+
+themeNumber.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    toggle(btn.textContent);
+  });
+});
+
+function setColor(c) {
+  logo.style.color = `${c}`;
+  themeTitle.style.color = `${c}`;
+  themeNumber.forEach((number) => {
+    number.style.color = `${c}`;
+  });
+}
+
+function toggle(i) {
+  if (i === "1") {
+    document.documentElement.setAttribute("data-theme", i);
+
+    toggle1.classList.remove("opacity");
+    toggle2.classList.add("opacity");
+    toggle3.classList.add("opacity");
+
+    setColor("white");
+  } else if (i === "2") {
+    document.documentElement.setAttribute("data-theme", i);
+
+    toggle1.classList.add("opacity");
+    toggle2.classList.remove("opacity");
+    toggle3.classList.add("opacity");
+
+    setColor("black");
+  } else if (i === "3") {
+    document.documentElement.setAttribute("data-theme", i);
+    setColor("yellow");
+
+    toggle1.classList.add("opacity");
+    toggle2.classList.add("opacity");
+    toggle3.classList.remove("opacity");
+  }
 }
